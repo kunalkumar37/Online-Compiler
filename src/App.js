7cb84251854d3a14d3d7cada4963a911b3d465fe
Kunal Kumar <61112064+kunalkumar37@users.noreply.github.com>
2022-06-25 23:48:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import {useState} from 'react';
+import './App.css';
+import Editor from '@monaco-editor/react';
+import Navbar from './Components/Navbar';
+import Axios from 'axios';
+import Spinner from 'react-svg-spinner';
+function App(){
+  const [userCode,setUserCode]=useState(``);
+  const [userLang, setUserLang]= useState("python");
+  const [userTheme,setUserTheme]=useState("vs-dark");
+  const [fontSize,setFontSize]=useState(20);
+
+  const [userInput,setUserInput]=useState("");
+  const [userOutput,setuserOutput]=useState("");
+
+  const [loading,setLoading]=useState(false);
+  const options={
+    fontSize:fontSize
+  }
+
+  function compile(){
+    setLoading(true);
+    if(userCode===``){
+      return
+    }
+    Axios.post(`http://localhost:8000/compile`,{
+      code:userCode,
+      language:userLang,
+      input:userInput
+    }).then((res)=>{
+      setuserOutput(res.data.output);
+
+    }).then(()=>{
+      setLoading(false);
+    })
+  }
+
+  function clearOutput(){
+    setuserOutput("");
+
+  }
+
+  return (
+    <div className="App">
+      <Navbar
+        userLang={userLang} setUserLang={setUserLang}
+        userTheme={userTheme} setUserTheme={setUserTheme}
+        fontSize={fontSize} setFontSize={setFontSize}
+      />
+      <div className="main">
+        <div className="left-container">
+          <Editor
+            options={options}
+            height="calc(100vh - 50px)"
+            width="100%"
+            theme={userTheme}
+            language={userLang}
+            defaultLanguage="python"
+            defaultValue="# Enter your code here"
+            onChange={(value) => { setUserCode(value) }}
+          />
+          <button className="run-btn" onClick={() => compile()}>
+             Run
+          </button>
+        </div>
+        <div className="right-container">
+          <h4>Input:</h4>
+          <div className="input-box">
+            <textarea id="code-inp" onChange=
+              {(e) => setUserInput(e.target.value)}>
+            </textarea>
+          </div>
+          <h4>Output:</h4>
+          {loading ? (
+            <div className="spinner-box">
+              <img src={Spinner} alt="Loading..." />
+            </div>
+          ) : (
+            <div className="output-box">
+              <pre>{userOutput}</pre>
+              <button onClick={() => { clearOutput() }}
+                 className="clear-btn">
+                 Clear
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default App;
